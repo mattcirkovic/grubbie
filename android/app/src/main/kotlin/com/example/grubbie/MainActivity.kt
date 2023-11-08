100a8dbaa65fcb03c182019fd007a12f440ada5c
mattcirkovic <matthewcirkovic@gmail.com>
2023-11-08 14:05:38 -0700

    init

---
+++ android/app/src/main/kotlin/com/example/grubbie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grubbie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:grubbie/app/homepage.dart';
+
+void main() {
+  runApp(const Grubbie());
+}
+
+class Grubbie extends StatelessWidget {
+  const Grubbie({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Grubbie',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
