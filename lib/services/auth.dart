e1462de5c4a96269b0ec7ea61d6fd25479de48c8
Matt Cirkovic <matthewcirkovic@gmail.com>
2023-11-20 10:54:28 -0700

    added authbase

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
-  User? get currentUser => FirebaseAuth.instance.currentUser;
+abstract class AuthBase {
+  User? get currentUser;
+  Future<User?> signInAnonomously();
+  Future<void> signOut();
+}
 
+class Auth implements AuthBase{
+
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  @override
   Future<User?> signInAnonomously() async {
-    final userCredential = await FirebaseAuth.instance.signInAnonymously();
+    final userCredential = await _firebaseAuth.signInAnonymously();
     return userCredential.user;
   }
 
+  @override
   Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut();
+    await _firebaseAuth.signOut();
   } 
 }
